69b43a5e5a5277669e6fe6d8a9fd8617d0e876b8
Michael O'Connell <michaeljoconnell@ufl.edu>
2021-08-15 20:02:32 -0400

    handle cases in which the region is extremely small

--- TryTryAgain/Source/Panels/ContentBrowser.cpp
+++ TryTryAgain/Source/Panels/ContentBrowser.cpp
@@ -32,64 +32,67 @@ void Panels::ContentBrowser::Draw()
 
 	float panelWidth = ImGui::GetContentRegionAvail().x;
 	int maxColumns = (int)(panelWidth / cellSize);
+	maxColumns = std::clamp(maxColumns, 1, 63);
 
 	ImGui::SameLine();
 	ImGui::Text(currentDirectory.string().c_str());
 
-	ImGui::BeginTable(currentDirectory.string().c_str(), maxColumns);
-	for (auto& directoryEntry : std::filesystem::directory_iterator(currentDirectory))
+	if (ImGui::BeginTable(currentDirectory.string().c_str(), maxColumns))
 	{
-		if (c < maxColumns)
+		for (auto& directoryEntry : std::filesystem::directory_iterator(currentDirectory))
 		{
-			ImGui::TableNextColumn();
-			c++;
-		}
-		else {
-			ImGui::TableNextRow();
-			c = 0;
-		}
-		
-		const auto& path = directoryEntry.path();
-		auto relativePath = std::filesystem::relative(path, assetPath);
-		auto fileNameString = relativePath.filename().string();
-
-		auto fileType = relativePath.extension().string();
-		fileType = fileType.erase(0, 1);
-
-		ImTextureID id;
-		if (directoryEntry.is_directory()) {
-			id = (ImTextureID) rm::ResourceManager::LoadTexture("Resources/Icons/folder.png", true, "Folder")->ID;
-		}
-		else {
-			if (std::filesystem::exists(FString("Resources/Icons/%s.png", fileType.c_str()).Text))
+			if (c < maxColumns)
 			{
-				id = (ImTextureID)rm::ResourceManager::LoadTexture(FString("Resources/Icons/%s.png", fileType.c_str()).Text, true, fileType)->ID;
+				ImGui::TableNextColumn();
+				c++;
 			}
-			else
+			else {
+				ImGui::TableNextRow();
+				c = 0;
+			}
+
+			const auto& path = directoryEntry.path();
+			auto relativePath = std::filesystem::relative(path, assetPath);
+			auto fileNameString = relativePath.filename().string();
+
+			auto fileType = relativePath.extension().string();
+			fileType = fileType.erase(0, 1);
+
+			ImTextureID id;
+			if (directoryEntry.is_directory()) {
+				id = (ImTextureID)rm::ResourceManager::LoadTexture("Resources/Icons/folder.png", true, "Folder")->ID;
+			}
+			else {
+				if (std::filesystem::exists(FString("Resources/Icons/%s.png", fileType.c_str()).Text))
+				{
+					id = (ImTextureID)rm::ResourceManager::LoadTexture(FString("Resources/Icons/%s.png", fileType.c_str()).Text, true, fileType)->ID;
+				}
+				else
+				{
+					id = (ImTextureID)rm::ResourceManager::LoadTexture("Resources/Icons/file.png", true, "File")->ID;
+				}
+
+			}
+
+			ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
+			// ImGui::Button(fileNameString.c_str(), { thumbnailSize, thumbnailSize });
+			ImGui::ImageButton(id, { thumbnailSize, thumbnailSize });
+			if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
 			{
-				id = (ImTextureID)rm::ResourceManager::LoadTexture("Resources/Icons/file.png", true, "File")->ID;
+				if (directoryEntry.is_directory())
+					currentDirectory /= path.filename();
 			}
-			
+			ImGui::PopStyleColor();
+
+			ImGui::TextWrapped(fileNameString.c_str());
 		}
 
-		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
-		ImGui::ImageButton(id, { thumbnailSize, thumbnailSize });
-		if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
-		{
-			if (directoryEntry.is_directory())
-				currentDirectory /= path.filename();
-		}
-		ImGui::PopStyleColor();
-
-		ImGui::TextWrapped(fileNameString.c_str());
+		ImGui::EndTable();
 	}
-
-	ImGui::EndTable();
+	
 
 	ImGui::SliderFloat("Thumbnail Size", &thumbnailSize, 16, 512);
 	ImGui::SliderFloat("Padding", &padding, 0, 32);
-
-	
 
 	ImGui::End();
 }

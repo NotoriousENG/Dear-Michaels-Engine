d1bb9e803d9ed34096d14592b42c744197cacb7b
Michael O'Connell <oconnellmj16@gmail.com>
2022-01-10 20:08:10 -0500

    fix compilation on windows

--- src/Modules/Render/GL_RenderModule.cpp
+++ src/Modules/Render/GL_RenderModule.cpp
@@ -114,50 +114,50 @@ unsigned int GL_RenderModule::GetTextureColorBuffer()
     return textureColorbuffer;
 }
 
-void GL_RenderModule::DrawMesh(rm::Mesh mesh, rm::Material material, rm::Shader shader)
-{
-    if (shader == material->Shader.get())
-    {
-        material->SetUniforms();
-
-        // bind appropriate textures
-        unsigned int diffuseNr = 1;
-        unsigned int specularNr = 1;
-        unsigned int normalNr = 1;
-        unsigned int heightNr = 1;
-        for (unsigned int i = 0; i < material->Textures.size(); i++)
-        {
-            glActiveTexture(GL_TEXTURE0 + i); // active proper texture unit before binding
-            // retrieve texture number (the N in diffuse_textureN)
-            std::string number;
-            std::string name = material->Textures[i]->type;
-            if (name == "texture_diffuse")
-                number = std::to_string(diffuseNr++);
-            else if (name == "texture_specular")
-                number = std::to_string(specularNr++); // transfer unsigned int to stream
-            else if (name == "texture_normal")
-                number = std::to_string(normalNr++); // transfer unsigned int to stream
-            else if (name == "texture_height")
-                number = std::to_string(heightNr++); // transfer unsigned int to stream
-
-            // now set the sampler to the correct texture unit
-            glUniform1i(glGetUniformLocation(shader->ID, (name + number).c_str()), i);
-            // and finally bind the texture
-            glBindTexture(GL_TEXTURE_2D, material->Textures[i]->ID);
-        }
-
-        if (material->Textures.size() == 0)
-        {
-            shader->SetBool("useTexture", false);
-            glBindTexture(GL_TEXTURE_2D, 0);
-        }
-    }
-
-    // draw mesh
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
-
-    // always good practice to set everything back to defaults once configured.
-    glActiveTexture(GL_TEXTURE0);
-}
+//void GL_RenderModule::DrawMesh(rm::Mesh mesh, rm::Material material, rm::Shader shader)
+//{
+//    if (shader == material->Shader.get())
+//    {
+//        material->SetUniforms();
+//
+//        // bind appropriate textures
+//        unsigned int diffuseNr = 1;
+//        unsigned int specularNr = 1;
+//        unsigned int normalNr = 1;
+//        unsigned int heightNr = 1;
+//        for (unsigned int i = 0; i < material->Textures.size(); i++)
+//        {
+//            glActiveTexture(GL_TEXTURE0 + i); // active proper texture unit before binding
+//            // retrieve texture number (the N in diffuse_textureN)
+//            std::string number;
+//            std::string name = material->Textures[i]->type;
+//            if (name == "texture_diffuse")
+//                number = std::to_string(diffuseNr++);
+//            else if (name == "texture_specular")
+//                number = std::to_string(specularNr++); // transfer unsigned int to stream
+//            else if (name == "texture_normal")
+//                number = std::to_string(normalNr++); // transfer unsigned int to stream
+//            else if (name == "texture_height")
+//                number = std::to_string(heightNr++); // transfer unsigned int to stream
+//
+//            // now set the sampler to the correct texture unit
+//            glUniform1i(glGetUniformLocation(shader->ID, (name + number).c_str()), i);
+//            // and finally bind the texture
+//            glBindTexture(GL_TEXTURE_2D, material->Textures[i]->ID);
+//        }
+//
+//        if (material->Textures.size() == 0)
+//        {
+//            shader->SetBool("useTexture", false);
+//            glBindTexture(GL_TEXTURE_2D, 0);
+//        }
+//    }
+//
+//    // draw mesh
+//    glBindVertexArray(VAO);
+//    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+//    glBindVertexArray(0);
+//
+//    // always good practice to set everything back to defaults once configured.
+//    glActiveTexture(GL_TEXTURE0);
+//}
--- src/Scene/Entity.h
+++ src/Scene/Entity.h
@@ -29,7 +29,7 @@ public:
     template<typename T>
     bool HasComponent()
     {
-        return scene->registry.any_of<T>(entityHandle);
+        return scene->registry.has<T>(entityHandle);
     }
 
     template<typename T>
